7e58de11554ab7301fae9b236f7fcf0254d3374d
afrianodev <andresfeliperianom@gmail.com>
2025-07-09 22:56:47 -0600

    Add the logic to the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("Hello world!!!");
+//Create a Rock Paper Scissors game against the computer
+
+//Create randomly the computer choice
+function getComputerChoice() {
+  let number = Math.floor(Math.random() * 3) + 1;
+  let choice;
+
+  if (number === 1) {
+    choice = "Rock";
+  } else if (number === 2) {
+    choice = "Paper";
+  } else {
+    choice = "Scissors";
+  }
+
+  return choice;
+}
+
+console.log(getComputerChoice());
